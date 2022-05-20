9bf5f40975f86db2ffe8b7541a7d2e797cd98b0d
mrsdno <christine@dno.dev>
2022-05-19 23:06:03 -0400

    set up inital file structure and database connection

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { appendFile } = require('fs');
+const db = require('./db/connection')
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+// not found response for unmatched routes
+app.use((req, res) => {
+    res.status(404).end();
+})
+
+// start server after db connection
+db.connect(err => {
+    if (err) throw err; 
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    })
+})
