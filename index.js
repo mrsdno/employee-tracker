6d95317c7c70665fbabcdb6fb9dc0c384f42e366
mrsdno <christine@dno.dev>
2022-05-25 00:33:02 -0400

    added inquirer prompt functionality to add employee

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const startApplication = () => {
                     })
                     .then(({ addedRoleTitle }) => {
                         // set up role object
-                        let role = new Role(addedRoleTitle);
+                        let role = new Role();
                         role.setRoleTitle(addedRoleTitle);
                         inquirer
                             .prompt({
@@ -98,6 +98,53 @@ const startApplication = () => {
                             })
                     })
                     
+                } else if (primarySelection === 'Add an employee') {
+                    inquirer
+                    .prompt({
+                        type: 'input',
+                        message: "What is the employee's first name?",
+                        name: 'addedEmployeeFirstName'
+                    })
+                    .then(({ addedEmployeeFirstName }) => {
+                        // set up employee object
+                        let employee = new Employee();
+                        employee.setFirstName(addedEmployeeFirstName);
+                        inquirer
+                            .prompt({
+                                type: 'input',
+                                message: "What is the employee's last name?",
+                                name: 'addedEmployeeLastName'
+                            })
+                            .then(({ addedEmployeeLastName }) => {
+                                employee.setLastName(addedEmployeeLastName);
+                                inquirer
+                                    .prompt({
+                                        type: 'list',
+                                        name: 'addedEmployeeRole',
+                                        message: "What is this employee's role?",
+                                        choices: ['1. Salesperson', '2. Lead Engineer', '3. Software Engineer', '4. Account Manager', '5. Accountant', '6. Legal Team Lead', '7. Lawyer']
+                                    })
+                                    .then(({ addedEmployeeRole }) => {
+                                        employee.setRoleId(addedEmployeeRole.charAt(0));
+                                        inquirer
+                                        .prompt({
+                                            type: 'list',
+                                            name: 'addedEmployeeManager',
+                                            message: "Who is this employee's manager?",
+                                            choices: ['1. Account Manager', '2. Lead Engineer', '3. Legal Team Lead']
+                                        })
+                                        .then(({ addedEmployeeManager }) => {
+                                            employee.setManagerId(addedEmployeeManager.charAt(0));
+                                            db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, [employee.first_name, employee.last_name, employee.role_id, employee.manager_id], (err, result) => {
+                                                if (err) {
+                                                    console.log(err);
+                                                }
+                                                console.log(employee.first_name, employee.last_name, employee.role_id, employee.manager_id);
+                                            });
+                                        })
+                                    });
+                            })
+                    })
                 }
             })
         }
